08bab4bded7e830a5327535389b3e39f9c9aa7ed
hongquan <lhqhacker@163.com>
2022-07-31 09:24:44 +0800

    refactor(store): 更改pinia使用方式

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,26 @@
-import { createPinia } from 'pinia';
+// import { createPinia } from 'pinia';
+import { useLoadingStore } from './loading';
+import { useResumeJsonStore, useResumeModelStore } from './resume';
+import { useUuidStore } from './uuid';
 
-const store = createPinia();
+// const store = createPinia();
+// export default store;
 
-export default store;
+export interface IAppStore {
+  useLoadingStore: ReturnType<typeof useLoadingStore>;
+  useResumeJsonStore: ReturnType<typeof useResumeJsonStore>;
+  useResumeModelStore: ReturnType<typeof useResumeModelStore>;
+  useUuidStore: ReturnType<typeof useUuidStore>;
+}
+
+const appStore: IAppStore = {} as IAppStore;
+
+/**
+ * 注册app状态库
+ */
+export const registerStore = () => {
+  appStore.useLoadingStore = useLoadingStore();
+  appStore.useResumeJsonStore = useResumeJsonStore();
+  appStore.useResumeModelStore = useResumeModelStore();
+  appStore.useUuidStore = useUuidStore();
+};
