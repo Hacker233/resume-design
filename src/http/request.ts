3ba4cb79437892ab5bed3e107ba842c83bb8370b
hongquan <lhqhacker@163.com>
2022-09-27 15:12:14 +0800

    perf(http): 请求拦截

--- src/http/request.ts
+++ src/http/request.ts
@@ -1,12 +1,33 @@
 import Request from './index';
 import { AxiosResponse } from 'axios';
 import CONFIG from '@/config/index';
+import appStore from '@/store';
 const http = new Request({
   baseURL: CONFIG.serverAddress,
   timeout: 1000 * 60 * 5,
   interceptors: {
     // 请求拦截器
-    requestInterceptors: (config) => config,
+    requestInterceptors: (config) => {
+      config.validateStatus = (status) => {
+        switch (status) {
+          case 401:
+            ElMessage.error('用户信息过期或无权限，请重新登录');
+            const { saveToken } = appStore.useTokenStore;
+            const { saveUserInfo } = appStore.useUserInfoStore;
+            const { setUuid } = appStore.useRefreshStore;
+            const router = useRouter();
+            saveToken(''); // 清除token
+            saveUserInfo(''); // 清除用户信息
+            setUuid(); // 全局刷新
+            router.push('/');
+            break;
+          default:
+            break;
+        }
+        return status >= 200 && status < 400;
+      };
+      return config;
+    },
     // 响应拦截器
     responseInterceptors: (result: AxiosResponse) => {
       return result;
