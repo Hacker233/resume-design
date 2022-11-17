1b857a50811bb04691660cb56a1ec26dc390d07c
hacker233 <lhqhacker@163.com>
2022-11-17 19:51:58 +0800

    feature(weebcode): 私有部署

---
+++ src/views/webCode/components/IntroduceCard.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="introduce-card-box"></div>
+</template>
+<style lang="scss" scoped>
+  .introduce-card-box {
+    width: 300px;
+    height: 350px;
+    box-shadow: 5px 5px 20px rgba(203, 205, 211, 0.5);
+    background-color: #fff;
+    border-radius: 6px;
+  }
+</style>
--- src/views/webCode/index.vue
+++ src/views/webCode/index.vue
@@ -1,17 +1,64 @@
 <template>
   <div class="web-code-box">
     <!-- 背景图 -->
-    <div class="bgc-box"></div>
+    <img class="bgc-img" src="@/assets/images/private_bgc.png" alt="" srcset="" />
+    <!-- 头部标题位置 -->
+    <div class="title-box">
+      <h1>将91化简私有部署为你自己的网站</h1>
+    </div>
+
+    <!-- 介绍卡片 -->
+    <div class="card-list-box">
+      <div v-for="(item, index) in 10" :key="index" class="card-box">
+        <introduce-card-vue></introduce-card-vue>
+      </div>
+    </div>
   </div>
 </template>
+<script lang="ts" setup>
+  import IntroduceCardVue from './components/IntroduceCard.vue';
+</script>
 <style lang="scss" scoped>
   .web-code-box {
     position: relative;
-    .bgc-box {
+    width: 1250px;
+    margin: 0 auto;
+    min-height: 500px;
+    z-index: 0;
+    .bgc-img {
       width: 100%;
+      height: 330px;
+      position: absolute;
+      z-index: 1;
+      filter: blur(3px);
+    }
+    .title-box {
+      position: relative;
+      z-index: 2;
+      display: flex;
+      justify-content: center;
       height: 300px;
-      background: url('@/assets/images/private_bgc.jpg') no-repeat;
-      background-size: 100% 100%;
+      align-items: center;
+      h1 {
+        color: #fff;
+        font-size: 60px;
+        font-family: '站酷快乐体';
+        letter-spacing: 2px;
+      }
+    }
+
+    .card-list-box {
+      display: flex;
+      justify-content: space-between;
+      flex-wrap: wrap;
+      margin: 80px 0 20px 0;
+      .card-box {
+        width: 33.33%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-bottom: 40px;
+      }
     }
   }
 </style>
