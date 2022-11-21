a332fb63fcccb467173820d2d58ab5f03d2208b4
hacker233 <lhqhacker@163.com>
2022-11-21 21:00:18 +0800

    feature(webcode): 私有部署

--- src/views/webCode/components/IntroduceCard.vue
+++ src/views/webCode/components/IntroduceCard.vue
@@ -1,12 +1,84 @@
 <template>
-  <div class="introduce-card-box"></div>
+  <div class="introduce-card-box">
+    <!-- 图标 -->
+    <div class="icon-box">
+      <svg-icon
+        :icon-name="cardData.iconfont"
+        class-name="icon-card"
+        color="#2ddd9d"
+        size="80px"
+      ></svg-icon>
+    </div>
+    <!-- 标题 -->
+    <div class="title-box">
+      <h1>{{ cardData.title }}</h1>
+    </div>
+    <!-- 简介 -->
+    <div class="introduce-box">
+      <p>{{ cardData.introduce }}</p>
+    </div>
+  </div>
 </template>
+<script lang="ts" setup>
+  interface TCard {
+    cardData: {
+      iconfont: string;
+      title: string;
+      introduce: string;
+    };
+  }
+  withDefaults(defineProps<TCard>(), {
+    cardData: () => {
+      return {
+        iconfont: '',
+        title: '',
+        introduce: ''
+      };
+    }
+  });
+</script>
 <style lang="scss" scoped>
   .introduce-card-box {
     width: 300px;
-    height: 350px;
+    height: 340px;
     box-shadow: 5px 5px 20px rgba(203, 205, 211, 0.5);
     background-color: #fff;
     border-radius: 6px;
+    display: flex;
+    flex-direction: column;
+    transition: all 00.3s;
+    &:hover {
+      box-shadow: 0px 16px 22px 2px rgb(0 37 58 / 14%);
+    }
+    .icon-box {
+      height: 60%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+    .title-box {
+      height: 15%;
+      display: flex;
+      align-items: flex-start;
+      justify-content: center;
+      h1 {
+        font-size: 22px;
+        color: #333;
+      }
+    }
+    .introduce-box {
+      padding: 0 15px;
+      flex: 1;
+      display: flex;
+      align-items: flex-start;
+      justify-content: center;
+      p {
+        font-size: 14px;
+        line-height: 1.5;
+        letter-spacing: 1px;
+        text-align: justify;
+        color: rgb(126, 126, 126);
+      }
+    }
   }
 </style>
--- src/views/webCode/index.vue
+++ src/views/webCode/index.vue
@@ -1,63 +1,119 @@
 <template>
   <div class="web-code-box">
     <!-- 背景图 -->
-    <img class="bgc-img" src="@/assets/images/private_bgc.png" alt="" srcset="" />
-    <!-- 头部标题位置 -->
-    <div class="title-box">
-      <h1>将91化简私有部署为你自己的网站</h1>
-    </div>
+    <div class="bgc-img"></div>
+    <!-- 内容区域 -->
+    <div class="content-bottom-box">
+      <!-- 头部标题位置 -->
+      <div class="title-box">
+        <h1>将91化简私有化部署为你自己的网站</h1>
+      </div>
 
-    <!-- 介绍卡片 -->
-    <div class="card-list-box">
-      <div v-for="(item, index) in 10" :key="index" class="card-box">
-        <introduce-card-vue></introduce-card-vue>
+      <!-- 介绍卡片 -->
+      <div class="card-list-box">
+        <div v-for="(item, index) in cardList" :key="index" class="card-box">
+          <introduce-card-vue :card-data="item"></introduce-card-vue>
+        </div>
       </div>
     </div>
   </div>
 </template>
 <script lang="ts" setup>
   import IntroduceCardVue from './components/IntroduceCard.vue';
+
+  // 卡片内容
+  const cardList = ref<Array<any>>([
+    {
+      iconfont: 'icon-codepen',
+      title: '整套的前端源码',
+      introduce: '91化简完整的前端源码，可直接运行使用并打包部署。'
+    },
+    {
+      iconfont: 'icon-_houduankaifa',
+      title: '完整的后端源码',
+      introduce: '91化简完整的后端源码，配置好数据库以及文件存储服务环境后可使用。'
+    },
+    {
+      iconfont: 'icon-database',
+      title: 'Mongodb数据库',
+      introduce: '91化简后台使用的测试数据库，提供相关测试数据。'
+    },
+    {
+      iconfont: 'icon-fuwuqi',
+      title: 'Minio文件存储',
+      introduce: '网站需要部署的minio的存储服务，手把手教你如何安装部署。'
+    },
+    {
+      iconfont: 'icon-duqumoban',
+      title: '超多简历模板',
+      introduce: '提供超多word简历模板，可根据自己的需求选择是否上传到网站。'
+    },
+    {
+      iconfont: 'icon-wendang',
+      title: '前端项目部署文档',
+      introduce: '全面的91化简前端项目部署上线的文档，学会此文档，可以让你快速入门部署前端项目。'
+    },
+    {
+      iconfont: 'icon-fuzhi',
+      title: '后端项目部署文档',
+      introduce: '教你如何将91化简后端项目部署到服务器，并且配置好数据库、邮件服务、minio等等。'
+    },
+    {
+      iconfont: 'icon-zhinan',
+      title: '项目开发指南',
+      introduce: '教你如何使用91化简项目，比如如何开发新的简历模板、开发后台接口等等。'
+    },
+    {
+      iconfont: 'icon-zuozhe',
+      title: '作者贴心服务',
+      introduce: '添加源作者微信，及时解决相关问题，给你贴心的服务。'
+    }
+  ]);
 </script>
 <style lang="scss" scoped>
   .web-code-box {
     position: relative;
-    width: 1250px;
-    margin: 0 auto;
+    width: 100%;
     min-height: 500px;
     z-index: 0;
+
     .bgc-img {
       width: 100%;
-      height: 330px;
+      height: 340px;
+      background-image: linear-gradient(to right, #24b27e, #14959b);
       position: absolute;
-      z-index: 1;
-      filter: blur(3px);
     }
-    .title-box {
-      position: relative;
-      z-index: 2;
-      display: flex;
-      justify-content: center;
-      height: 300px;
-      align-items: center;
-      h1 {
-        color: #fff;
-        font-size: 60px;
-        font-family: '站酷快乐体';
-        letter-spacing: 2px;
-      }
-    }
-
-    .card-list-box {
-      display: flex;
-      justify-content: space-between;
-      flex-wrap: wrap;
-      margin: 80px 0 20px 0;
-      .card-box {
-        width: 33.33%;
+    .content-bottom-box {
+      width: 1250px;
+      margin: 0 auto;
+      .title-box {
+        position: relative;
+        z-index: 2;
         display: flex;
-        align-items: center;
         justify-content: center;
-        margin-bottom: 40px;
+        height: 300px;
+        align-items: center;
+        color: white;
+        h1 {
+          color: #fff;
+          font-size: 60px;
+          font-family: '站酷快乐体';
+          letter-spacing: 2px;
+        }
+      }
+
+      .card-list-box {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+        margin: 80px 0 20px 0;
+        .card-box {
+          width: 33.33%;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          margin-bottom: 40px;
+        }
       }
     }
   }
