b1fe9a48678e71c4492ae2f863bae6dd3760d94c
hacker233 <lhqhacker@163.com>
2023-09-12 19:50:58 +0800

    feature(admin): 添加总收入统计

--- src/views/admin/panel/components/UserStatic.vue
+++ src/views/admin/panel/components/UserStatic.vue
@@ -6,7 +6,7 @@
         <svg-icon
           icon-name="icon-renwu-ren"
           class-name="juejin"
-          size="70px"
+          size="60px"
           color="#fff"
         ></svg-icon>
       </div>
@@ -20,7 +20,7 @@
         <svg-icon
           icon-name="icon-xinzengyonghu"
           class-name="juejin"
-          size="70px"
+          size="60px"
           color="#fff"
         ></svg-icon>
       </div>
@@ -34,7 +34,7 @@
         <svg-icon
           icon-name="icon-yanzhengyanzhengma"
           class-name="juejin"
-          size="70px"
+          size="60px"
           color="#fff"
         ></svg-icon>
       </div>
@@ -48,7 +48,7 @@
         <svg-icon
           icon-name="icon-yanzhengyanzhengma"
           class-name="juejin"
-          size="70px"
+          size="60px"
           color="#fff"
         ></svg-icon>
       </div>
@@ -57,18 +57,34 @@
         <p>已验证验证邮箱总数</p>
       </div>
     </div>
+    <div class="add-user-box">
+      <div class="left">
+        <svg-icon
+          icon-name="icon-nianduxinchou"
+          class-name="juejin"
+          size="60px"
+          color="#fff"
+        ></svg-icon>
+      </div>
+      <div class="right">
+        <h1>{{ panelData.income }}元</h1>
+        <p>总收入</p>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts" setup>
   import { getUserPanelAsync } from '@/http/api/panel';
 
   interface IUserPanel {
+    income: number;
     todayUsers: number;
     unVaildEamil: number;
     usersTotal: number;
     validEmail: number;
   }
   const panelData = reactive<IUserPanel>({
+    income: 0,
     todayUsers: 0,
     unVaildEamil: 0,
     usersTotal: 0,
@@ -77,6 +93,7 @@
   const getUserPanel = async () => {
     const data = await getUserPanelAsync();
     if (data.data.status === 200) {
+      panelData.income = data.data.data.income;
       panelData.todayUsers = data.data.data.todayUsers;
       panelData.unVaildEamil = data.data.data.unVaildEamil;
       panelData.usersTotal = data.data.data.usersTotal;
@@ -91,6 +108,7 @@
   .user-statistic-box {
     display: flex;
     justify-content: space-between;
+    flex-wrap: wrap;
     .add-user-box {
       border-radius: 8px;
       overflow: hidden;
@@ -98,6 +116,7 @@
       width: 24%;
       box-shadow: rgba(0, 0, 0, 0.117647) 0px 1px 6px, rgba(0, 0, 0, 0.117647) 0px 1px 4px;
       display: flex;
+      margin-bottom: 30px;
       .left {
         width: 20%;
         background-color: rgb(105, 143, 105);
@@ -113,11 +132,11 @@
         align-items: center;
         justify-content: center;
         h1 {
-          font-size: 40px;
+          font-size: 30px;
           margin-bottom: 10px;
         }
         p {
-          font-size: 16px;
+          font-size: 14px;
         }
       }
     }
