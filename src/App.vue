bb5397e5e4948559b3451391fc11b9abc77b7723
hacker233 <lhqhacker@163.com>
2024-06-28 22:03:43 +0800

    deploy(docker): dockerfile

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@
   import appStore from './store';
   import { storeToRefs } from 'pinia';
   // import { openAndCloseLoadingByTime } from './utils/common';
-  import zhCn from 'element-plus/lib/locale/lang/zh-cn';
+  import zhCn from 'element-plus/es/locale/lang/zh-cn';
   import { addWebsiteViewsAsync } from './http/api/panel';
   const { isLoading } = storeToRefs(appStore.useLoadingStore);
   // openAndCloseLoadingByTime(1500); // 等待动画层
