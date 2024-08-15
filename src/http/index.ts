41010482bc494b547916bf297c15a2bb1f97b918
hacker233 <lhqhacker@163.com>
2024-08-15 21:32:31 +0800

    deploy(config): 优化代码以及环境区分

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,194 +1,138 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosResponse, AxiosError, AxiosRequestConfig, AxiosInstance } from 'axios';
+import type { RequestConfig, RequestInterceptors } from './types/types';
 import qs from 'qs';
-import type { AxiosInstance, AxiosRequestConfig } from 'axios';
-import type { RequestConfig, RequestInterceptors, CancelRequestSource } from './types/types';
 
 class Request {
-  // axios 实例
   instance: AxiosInstance;
-  // 拦截器对象
   interceptorsObj?: RequestInterceptors<AxiosResponse>;
-
-  /*
-  存放取消方法的集合
-  * 在创建请求后将取消请求方法 push 到该集合中
-  * 封装一个方法，可以取消请求，传入 url: string|string[]  
-  * 在请求之前判断同一URL是否存在，如果存在就取消请求
-  */
-  cancelRequestSourceList?: CancelRequestSource[];
-  /*
-  存放所有请求URL的集合
-  * 请求之前需要将url push到该集合中
-  * 请求完毕后将url从集合中删除
-  * 添加在发送请求之前完成，删除在响应之后删除
-  */
-  requestUrlList?: string[];
+  cancelRequestSourceList: Map<string, AbortController> = new Map();
+  requestUrlList: Set<string> = new Set();
 
   constructor(config: RequestConfig) {
-    this.requestUrlList = [];
-    this.cancelRequestSourceList = []; // 取消请求列表
-    this.instance = axios.create(config); // 创建axios实例
+    this.instance = axios.create(config);
     this.interceptorsObj = config.interceptors;
-    // 拦截器执行顺序 接口请求 -> 实例请求 -> 全局请求 -> 实例响应 -> 全局响应 -> 接口响应
+
     this.instance.interceptors.request.use(
-      (res: AxiosRequestConfig) => {
-        return res;
+      (config: AxiosRequestConfig) => {
+        // 在此处添加请求拦截逻辑
+        return config;
       },
-      (err: any) => err
+      (error: AxiosError) => {
+        return Promise.reject(error);
+      }
     );
 
-    // 使用实例拦截器
-    this.instance.interceptors.request.use(
-      this.interceptorsObj?.requestInterceptors,
-      this.interceptorsObj?.requestInterceptorsCatch
-    );
+    if (this.interceptorsObj) {
+      this.instance.interceptors.request.use(
+        this.interceptorsObj.requestInterceptors,
+        this.interceptorsObj.requestInterceptorsCatch
+      );
+      this.instance.interceptors.response.use(
+        this.interceptorsObj.responseInterceptors,
+        this.interceptorsObj.responseInterceptorsCatch
+      );
+    }
+
     this.instance.interceptors.response.use(
-      this.interceptorsObj?.responseInterceptors,
-      this.interceptorsObj?.responseInterceptorsCatch
-    );
-    // 全局响应拦截器保证最后执行
-    this.instance.interceptors.response.use(
-      // 因为我们接口的数据都在res.data下，所以我们直接返回res.data
-      (res: AxiosResponse) => {
-        return res.data;
-      },
-      (error: any) => {
-        if (error && error.response) {
-          switch (error.response.status) {
-            case 400:
-              error.message = '请求错误(400)';
-              break;
-            case 401:
-              error.message = '未授权，请重新登录(401)';
-              break;
-            case 403:
-              error.message = '拒绝访问(403)';
-              break;
-            case 404:
-              error.message = '请求出错(404)';
-              break;
-            case 408:
-              error.message = '请求超时(408)';
-              break;
-            case 500:
-              error.message = '服务器错误(500)';
-              break;
-            case 501:
-              error.message = '服务未实现(501)';
-              break;
-            case 502:
-              error.message = '网络错误(502)';
-              break;
-            case 503:
-              error.message = '服务不可用(503)';
-              break;
-            case 504:
-              error.message = '网络超时(504)';
-              break;
-            case 505:
-              error.message = 'HTTP版本不受支持(505)';
-              break;
-            default:
-              error.message = `连接出错(${error.response.status})!`;
-          }
-        } else {
-          error.message = '连接服务器失败!';
-        }
-        console.log('error', error.response);
-        ElMessage.error(error.response.data.message || error.message);
+      (response: AxiosResponse) => response.data,
+      (error: AxiosError) => {
+        // 错误处理
+        const status = error.response?.status;
+        const errorMessages: Record<number, string> = {
+          400: '请求错误(400)',
+          401: '未授权，请重新登录(401)',
+          403: '拒绝访问(403)',
+          404: '请求出错(404)',
+          408: '请求超时(408)',
+          500: '服务器错误(500)',
+          501: '服务未实现(501)',
+          502: '网络错误(502)',
+          503: '服务不可用(503)',
+          504: '网络超时(504)',
+          505: 'HTTP版本不受支持(505)'
+        };
+        const message = errorMessages[status as number] || `连接出错(${status})!`;
+        console.error('HTTP Error:', message, error.response);
         return Promise.reject(error);
       }
     );
   }
-  /**
-   * @description: 获取指定 url 在 cancelRequestSourceList 中的索引
-   * @param {string} url
-   * @returns {number} 索引位置
-   */
+
   private getSourceIndex(url: string): number {
-    return this.cancelRequestSourceList?.findIndex((item: CancelRequestSource) => {
-      return Object.keys(item)[0] === url;
-    }) as number;
+    return Array.from(this.cancelRequestSourceList.keys()).indexOf(url);
   }
-  /**
-   * @description: 删除 requestUrlList 和 cancelRequestSourceList
-   * @param {string} url
-   * @returns {*}
-   */
+
   private delUrl(url: string) {
-    const urlIndex = this.requestUrlList?.findIndex((u) => u === url);
-    const sourceIndex = this.getSourceIndex(url);
-    // 删除url和cancel方法
-    urlIndex !== -1 && this.requestUrlList?.splice(urlIndex as number, 1);
-    sourceIndex !== -1 && this.cancelRequestSourceList?.splice(sourceIndex as number, 1);
+    this.requestUrlList.delete(url);
+    this.cancelRequestSourceList.delete(url);
   }
+
   request<T>(config: RequestConfig<T>): Promise<T> {
     return new Promise((resolve, reject) => {
-      // 如果我们为单个请求设置拦截器，这里使用单个请求的拦截器
       if (config.interceptors?.requestInterceptors) {
         config = config.interceptors.requestInterceptors(config);
       }
+
       const url = config.url;
-      // url存在保存取消请求方法和当前请求url
+      const controller = new AbortController();
+      const timeoutId = setTimeout(() => {
+        controller.abort(); // 超时取消请求
+      }, 30000); // 30秒
+
       if (url) {
-        this.requestUrlList?.push(url);
-        // TODO 在axios0.22起，对CancelToken已经弃用，需要改成  AbortController 文档：https://axios-http.com/docs/cancellation
-        config.cancelToken = new axios.CancelToken((c) => {
-          this.cancelRequestSourceList?.push({
-            [url]: c
-          });
-        });
+        this.requestUrlList.add(url);
+        this.cancelRequestSourceList.set(url, controller);
       }
+
+      config.signal = controller.signal;
+
       if (!config.headers) {
         config.headers = {};
         config.headers['Content-Type'] = 'application/json';
       }
 
-      // 序列化
       if (config.method === 'post' && config.headers['Content-Type'] !== 'application/json') {
-        config.data = qs.stringify(config.data); //序列化post 参数
+        config.data = qs.stringify(config.data);
       }
 
-      // 设置token
       if (localStorage.getItem('token')) {
         config.headers.Authorization = localStorage.getItem('token') as string;
       }
+
       this.instance
         .request<any, T>(config)
         .then((res) => {
-          // 如果我们为单个响应设置拦截器，这里使用单个响应的拦截器
+          clearTimeout(timeoutId); // 请求成功，清除定时器
           if (config.interceptors?.responseInterceptors) {
             res = config.interceptors.responseInterceptors(res);
           }
           resolve(res);
         })
-        .catch((err: any) => {
-          reject(err);
+        .catch((error: AxiosError) => {
+          clearTimeout(timeoutId); // 请求失败，清除定时器
+          reject(error);
         })
         .finally(() => {
-          url && this.delUrl(url);
+          if (url) this.delUrl(url);
         });
     });
   }
-  // 取消请求
+
   cancelRequest(url: string | string[]) {
     if (typeof url === 'string') {
-      // 取消单个请求
-      const sourceIndex = this.getSourceIndex(url);
-      sourceIndex >= 0 && this.cancelRequestSourceList?.[sourceIndex][url]();
+      const controller = this.cancelRequestSourceList.get(url);
+      controller?.abort();
     } else {
-      // 存在多个需要取消请求的地址
       url.forEach((u) => {
-        const sourceIndex = this.getSourceIndex(u);
-        sourceIndex >= 0 && this.cancelRequestSourceList?.[sourceIndex][u]();
+        const controller = this.cancelRequestSourceList.get(u);
+        controller?.abort();
       });
     }
   }
-  // 取消全部请求
+
   cancelAllRequest() {
-    this.cancelRequestSourceList?.forEach((source) => {
-      const key = Object.keys(source)[0];
-      source[key]();
+    this.cancelRequestSourceList.forEach((controller) => {
+      controller.abort();
     });
   }
 }
--- src/http/request.ts
+++ src/http/request.ts
@@ -3,35 +3,41 @@ import { AxiosResponse } from 'axios';
 import CONFIG from '@/config/index';
 import appStore from '@/store';
 import LoginDialog from '@/components/LoginDialog/LoginDialog';
+import { ElMessage } from 'element-plus';
+
+const handleUnauthorized = () => {
+  ElMessage.error('用户信息过期或无权限，请重新登录');
+  const { saveToken } = appStore.useTokenStore;
+  const { saveUserInfo } = appStore.useUserInfoStore;
+  const { setUuid } = appStore.useRefreshStore;
+
+  saveToken(''); // 清除token
+  saveUserInfo(''); // 清除用户信息
+  setUuid(); // 全局刷新
+  LoginDialog(true); // 显示登录对话框
+};
+
 const http = new Request({
   baseURL: CONFIG.serverAddress,
   timeout: 1000 * 60 * 5,
   interceptors: {
-    // 请求拦截器
     requestInterceptors: (config) => {
       config.validateStatus = (status) => {
-        switch (status) {
-          case 401:
-            ElMessage.error('用户信息过期或无权限，请重新登录');
-            const { saveToken } = appStore.useTokenStore;
-            const { saveUserInfo } = appStore.useUserInfoStore;
-            const { setUuid } = appStore.useRefreshStore;
-            saveToken(''); // 清除token
-            saveUserInfo(''); // 清除用户信息
-            setUuid(); // 全局刷新
-            LoginDialog(true);
-            break;
-          default:
-            break;
+        if (status === 401) {
+          handleUnauthorized();
         }
         return status >= 200 && status < 400;
       };
       return config;
     },
-    // 响应拦截器
-    responseInterceptors: (result: AxiosResponse) => {
-      return result;
+    responseInterceptors: (response: AxiosResponse) => {
+      return response;
+    },
+    responseInterceptorsCatch: (error: any) => {
+      ElMessage.error(error.message || '请求失败，请稍后再试');
+      return Promise.reject(error);
     }
   }
 });
+
 export default http;
