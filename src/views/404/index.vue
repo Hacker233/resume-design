5c5c5e7ab02d07e3c3f4db0d57245030d108213b
hacker233 <lhqhacker@163.com>
2025-01-27 15:20:55 +0800

    feature(404): 添加404页面

--- src/views/404/index.vue
+++ src/views/404/index.vue
@@ -2,8 +2,13 @@
   <div class="not-found">
     <div class="container">
       <h1>404</h1>
-      <p>天啦! 你访问了一个不存在的页面！</p>
-      <router-link to="/" class="home-button">回首页</router-link>
+      <p>我的天啦~ 你来到了未知领域！</p>
+      <router-link to="/" class="home-button">我要回家</router-link>
+    </div>
+    <div class="background-circles">
+      <div class="circle circle1"></div>
+      <div class="circle circle2"></div>
+      <div class="circle circle3"></div>
     </div>
   </div>
 </template>
@@ -22,10 +27,13 @@
     height: 100vh;
     background-color: #f5f5f5;
     font-family: Arial, sans-serif;
+    position: relative;
+    overflow: hidden;
   }
 
   .container {
     text-align: center;
+    z-index: 1;
   }
 
   .container h1 {
@@ -53,5 +61,55 @@
 
   .home-button:hover {
     background-color: #0056b3;
+  }
+
+  .background-circles {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    pointer-events: none;
+  }
+
+  .circle {
+    position: absolute;
+    border-radius: 50%;
+    background-color: rgba(0, 123, 255, 0.1);
+    animation: float 10s infinite ease-in-out;
+  }
+
+  .circle1 {
+    width: 100px;
+    height: 100px;
+    top: 20%;
+    left: 10%;
+    animation-delay: 0s;
+  }
+
+  .circle2 {
+    width: 150px;
+    height: 150px;
+    top: 60%;
+    left: 30%;
+    animation-delay: 2s;
+  }
+
+  .circle3 {
+    width: 200px;
+    height: 200px;
+    top: 40%;
+    left: 70%;
+    animation-delay: 4s;
+  }
+
+  @keyframes float {
+    0%,
+    100% {
+      transform: translateY(0);
+    }
+    50% {
+      transform: translateY(-20px);
+    }
   }
 </style>
