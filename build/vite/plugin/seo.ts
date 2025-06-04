5406649af969c692960d255f5af285ebfc3924b9
hacker233 <lhqhacker@163.com>
2025-06-04 21:02:28 +0800

    deploy(seo): seo

--- build/vite/plugin/seo.ts
+++ build/vite/plugin/seo.ts
@@ -1,5 +1,5 @@
 // 全局SEO主要内容
-export const title = '猫步简历 | 开源免费AI智能简历生成制作 | PDF/JSON/MD多格式导出';
+export const title = '猫步简历 - 开源免费 | AI智能简历生成器';
 export const keywords =
   '开源免费AI简历生成器,AI简历生成,免费简历制作工具,PDF简历导出,JSON简历模板,deepseek简历优化,AI智能润色简历,AI简历诊断,Markdown导出';
 export const description =
